581fa5eccbd78b68b91054c22bfb3ea4e964e03c
Mohib Azam <mazam@redhat.com>
2025-10-10 14:31:26 -0400

    Polished up the documentation and added verbosity feature for the PR

--- src/training_hub/profiling/memory_estimator.py
+++ src/training_hub/profiling/memory_estimator.py
@@ -8,9 +8,7 @@ from transformers.models.perceiver.modeling_perceiver import PerceiverMultimodal
 Code assisted by Cursor/Claude4
 """
 
-# TODO: Make this a class structure to make this more extendable
-# TODO: Make a notebook 
-
+# Constants that specify
 FLOAT32_BYTES_N: int = 4
 FLOAT16_BYTES_N: int = 2
 FLOAT8_BYTES_N: int = 1
@@ -19,20 +17,25 @@ ADAMW_PARAMS_N: int = 2
 # Helper lambda to do the rounding when printing 
 ROUNDER = lambda value : str(round(value / 1073741824, 1))
 
-
 class BasicEstimator:
     """
+    An estimator for the memory usage of a typical LLM training pass via SFT.
+    Estimators for other methods can subclass this class.
+
     Args:
-            self.num_gpus: Number of GPUs to use for training (default: 8)
-            gpu_memory: The VRAM of each GPU in bytes (default: 85899345920 for 80 GB)
-            model_path: Path to the model to fine-tune, namely a HuggingFace
-                        model path (default: "ibm-granite/granite-3.3-8b-instruct")
-            self.effective_batch_size: The number of samples in a minibatch that the model has to see before backpropping.
-            max_seq_len: Maximum sequence length of dataset samples 
-            max_tokens_per_gpu: The maximum number of tokens that can be placed
+        num_gpus (int): Number of GPUs to use for training (default: 8)
+        gpu_memory (int): The VRAM of each GPU in bytes (default: 85899345920 for 80 GB)
+        model_path (str): HuggingFace model path to the model to fine-tune
+                        (default: "ibm-granite/granite-3.3-8b-instruct")
+        effective_batch_size (int): The number of samples in a minibatch that the model
+                                    has to see before backpropping.
+        max_seq_len (int): Maximum sequence length of dataset samples 
+        max_tokens_per_gpu (int): The maximum number of tokens that can be placed
                                 on a single GPU during each mini-batch.
-            using_osft: If set to True, calculate the memory usage assuming the
-                        model is trained via OSFT. If False, assume SFT. 
+        use_liger (bool): If true, estimate assuming Liger Kernels are used.
+        verbose (int): The level of verbosity to print out. Set to 0 for no printing,
+                        set to 1 to print out only hardware recommendations,
+                        set to 2 for a detailed memory breakdown.
     """
 
     def __init__(
@@ -44,11 +47,13 @@ class BasicEstimator:
         max_seq_len: int = None,
         max_tokens_per_gpu: int = None,
         use_liger: bool = False,
+        verbose: int = 1,
     ):
         self.num_gpus = num_gpus
         self.gpu_memory = gpu_memory
         self.model_path = model_path
         self.use_liger = use_liger
+        self.verbose = verbose
         
         # Load model directly
         self.model = AutoModel.from_pretrained(model_path, trust_remote_code=True)
@@ -56,68 +61,100 @@ class BasicEstimator:
         # Determine parameters needed for calculations
         self.num_params: int = self.model.num_parameters(only_trainable=False)
         self.num_trainable_params: int = self.model.num_parameters(only_trainable=True)
-
-        # TODO: Use model._modules if the config can't give us the information we need
-        # TODO: Handle the fact that gemma has that language model distinction -_-
         self.num_layers: int = self.model.config.num_hidden_layers
         self.hidden_size: int = self.model.config.hidden_size  
 
         # The number of tokens on each GPU will be bounded by either
         # The largest number of tokens in a batch (divided by # GPUs)
         # *or* the value of max_tokens_per_gpu
-        # TODO: Should we try to bound this based on the expected dataset's lengths?
         if effective_batch_size is None or max_seq_len is None:
             self.tokens_per_gpu: int = max_tokens_per_gpu
         elif max_tokens_per_gpu is None:
             self.tokens_per_gpu: int = effective_batch_size * max_seq_len / self.num_gpus
         else:
-            self.tokens_per_gpu: int = min(max_tokens_per_gpu, effective_batch_size * max_seq_len / self.num_gpus)
+            self.tokens_per_gpu: int = min(max_tokens_per_gpu,
+                                            effective_batch_size * max_seq_len / self.num_gpus)
 
         # This is a scalar that's applied during the output memory calculation
         self.output_constant = 8/3
         self.main_dtype_bytes = FLOAT32_BYTES_N
         self.opt_params = ADAMW_PARAMS_N
 
+
     def _calc_model_params(self):
+        """
+        Calcuate the VRAM for storing the model parameters
+        """
         return self.num_params * self.main_dtype_bytes / self.num_gpus
 
+
     def _calc_gradients(self):
+        """
+        Calcuate the VRAM for storing the gradients
+        """
         return self.main_dtype_bytes * self.num_trainable_params / self.num_gpus
 
+
     def _calc_optimizer(self):
+        """
+        Calculate the VRAM for storing the optimizer states
+        """
         return self.main_dtype_bytes * self.num_trainable_params * self.opt_params / self.num_gpus
 
+
     def _calc_intermediate_activations(self):
+        """
+        Calculate the VRAM for storing the model's intermediate activations
+        """
         return (self.tokens_per_gpu * self.main_dtype_bytes  * self.num_layers * self.hidden_size)
 
+
     def _calc_outputs(self):
-        # Liger removes the need to store the activated outputs of the model
-        # If we're not using Liger, factor this in.
+        """
+        Calculate the VRAM for storing the model's activated outputs
+        """
         if not self.use_liger:
-            # TODO: This constant is how many times the output tensor gets
-            # repeated empirically. Why is this happening...?
             return (self.tokens_per_gpu * self.main_dtype_bytes * self.model.embed_tokens.num_embeddings) * self.output_constant
         else:
             return 0
 
+
     def _calc_additional(self, **kwargs):
+        """
+        Calculate any additional VRAM that this training method might need
+        """
         return 0
 
+
     def _apply_overhead(self, subtotal):
-        # Perform those calculations using the lambdas!
+        """
+        Apply 0-30% overhead to the subtotal of VRAM needed
+
+        Args:
+            subtotal (int): The subtotal of VRAM needed
+
+        Returns:
+            gpu_tuple: A triplet of the lower bound, the specific estimate,
+                        and upper bound of VRAM required to train this model
+            overhead_tuple: A triplet of the lowest, expected, and highest amount of
+                            overhead memory required for training
+        """
         gpu_vram_total_low: int = int(subtotal)
         gpu_vram_total_mid: int = int(1.1 *  subtotal)
         gpu_vram_total_high: int = int(1.3 * subtotal)
         extra_low: int = max(gpu_vram_total_low - subtotal, 0)
         extra_mid: int = gpu_vram_total_mid - subtotal
         extra_high: int = gpu_vram_total_high - subtotal
-        return(gpu_vram_total_low, gpu_vram_total_mid, gpu_vram_total_high), (extra_low, extra_mid, extra_high)
+        return(gpu_vram_total_low, gpu_vram_total_mid, gpu_vram_total_high), \
+                (extra_low, extra_mid, extra_high)
 
 
-    def _print_results(self, results, overhead, gpu_vram_par, gpu_vram_opt, gpu_vram_grad, gpu_vram_act, gpu_vram_outputs, gpu_vram_additional):
+    def _print_results(self, results, overhead, gpu_vram_par, gpu_vram_opt,
+                        gpu_vram_grad, gpu_vram_act, gpu_vram_outputs, gpu_vram_additional):
             """
             Print out a breakdown of the estimated memory requirements
             """
+
             print("Estimations for " + self.model_path + ":\n\n")
 
             print("Summary:")
@@ -146,8 +183,11 @@ class BasicEstimator:
 
 
     def _print_tips(self, results):
-
-        # TODO: Can we fit in param recommendations?
+        """
+        Print out information on how feasible it is to train the model on
+        the user's given hardware and, if necessary, recommend the necessary
+        amount of additional memory. 
+        """
 
         min_message = "Minimum extra memory required (to reach the\nlow bound of memory requirements): " + \
             ROUNDER(results[0] - self.gpu_memory) + " GB"
@@ -188,39 +228,49 @@ class BasicEstimator:
         in which all data types are float32 and the optimizer is always AdamW.
 
         Return:
-            lower_bound (int): The lower bound of the memory usage (in bytes)
-            expected (int): The expected amount of memory usage (in bytes)
-            upper_bound (int): The upper bound of the memory usage (in bytes)
+            A tuple containing three values:
+                lower_bound (int): The lower bound of the memory usage (in bytes)
+                expected (int): The expected amount of memory usage (in bytes)
+                upper_bound (int): The upper bound of the memory usage (in bytes)
         """  
 
-        # Calculate the amount of VRAM needed to fine-tune the provided model
-        # TODO: Validate this            
+        # Calculate each piece of memory to be factored into the estimation
         gpu_vram_par = self._calc_model_params()
-
-        # VALIDATED FOR ACCURACY:
         gpu_vram_opt: int = self._calc_optimizer()
         gpu_vram_grad: int = self._calc_gradients()
-
-        # The VRAM needed to store the intermediate activations of the model
-        # TODO: Validate this
         gpu_vram_act: int = self._calc_intermediate_activations()
         gpu_vram_outputs: int = self._calc_outputs()
         gpu_vram_additional: int = self._calc_additional()
 
-        subtotal: int = gpu_vram_par + gpu_vram_opt + gpu_vram_grad + gpu_vram_act + gpu_vram_outputs + gpu_vram_additional
+        # Sum up each proposed amount of memory
+        subtotal: int = gpu_vram_par + \
+                        gpu_vram_opt + \
+                        gpu_vram_grad + \
+                        gpu_vram_act + \
+                        gpu_vram_outputs + \
+                        gpu_vram_additional
+    
+        # Apply some proportion of overhead to get the final memory calculations
         results, overhead = self._apply_overhead(subtotal)
 
-        # Print out the recommendations based on the calculated memory requirements
-        # and the provided GPU memory
-        self._print_results(results, overhead, gpu_vram_par, gpu_vram_opt, gpu_vram_grad, gpu_vram_act, gpu_vram_outputs, gpu_vram_additional)
-        self._print_tips(results)
+        # Print out details of the memory breakdown and recommendations based
+        # on the user's given hardware. 
+        if self.verbose > 1:
+            self._print_results(results, overhead, gpu_vram_par,
+                                gpu_vram_opt, gpu_vram_grad, gpu_vram_act,
+                                gpu_vram_outputs, gpu_vram_additional)
+        if self.verbose > 0:
+            self._print_tips(results)
 
         # Return the lower bound, estimated value, and upper bound 
         return results 
 
 
-
 class OSFTEstimator(BasicEstimator):
+    """
+    An estimator for the memory usage of an LLM trained via OSFT. 
+    Subclasses the BasicEstimator class.
+    """
 
     @override
     def __init__(
@@ -232,8 +282,11 @@ class OSFTEstimator(BasicEstimator):
         max_seq_len: int = None,
         max_tokens_per_gpu: int = None,
         use_liger: bool = False,
+        verbose: int = 1,
     ):
-        super().__init__(num_gpus, gpu_memory, model_path, effective_batch_size, max_seq_len, max_tokens_per_gpu, use_liger)
+        super().__init__(num_gpus, gpu_memory, model_path,
+                        effective_batch_size, max_seq_len, max_tokens_per_gpu, 
+                        use_liger, verbose)
         self.output_constant = 7/3
 
 
@@ -245,14 +298,9 @@ class OSFTEstimator(BasicEstimator):
         For the given layer, determine how many bytes would be needed
         to store this layer when trained with OSFT
         """
+
         # If the layer is trainable, i.e. it contains any of these terms,
         # then we will need to store a SVD decomposition of the layer in memory.
-
-        # TODO: Okay, so according to Nikhil, we don't need the S tensor. 
-        # Also, check through your profiles to see if you can hammer some stronger estimates
-        # Maybe we should account for the fact that these matrices sometimes get offloaded
-        # during the activation stage...?
-
         TARGET_TERMS: list[str] = ['self_attn.q_proj',
                                 'self_attn.k_proj',
                                 'self_attn.v_proj',
@@ -266,7 +314,8 @@ class OSFTEstimator(BasicEstimator):
         for term in TARGET_TERMS:
             if layer_name.find(term) > -1 and layer_name.find('weight') > -1:
                 U_bytes_n: int = layer_data.shape[0] * layer_data.shape[0] 
-                S_bytes_n: int = layer_data.shape[0] if layer_data.shape[0] < layer_data.shape[1] else layer_data.shape[1]
+                S_bytes_n: int = layer_data.shape[0] if layer_data.shape[0] < \
+                            layer_data.shape[1] else layer_data.shape[1]
                 V_bytes_n: int = layer_data.shape[1] * layer_data.shape[1]
                 return (U_bytes_n + S_bytes_n + V_bytes_n) * FLOAT32_BYTES_N
 
@@ -285,13 +334,17 @@ class OSFTEstimator(BasicEstimator):
             total_bytes += self._check_layer_params(layer_name)
         return total_bytes
 
+
     @override
     def _calc_model_params(self):
-         return self._calc_osft_params() / self.num_gpus
+        """
+        Override the model parameter calculation by calculating based on OSFT's parameters
+        """
+        return self._calc_osft_params() / self.num_gpus
 
 
 def estimate(
-        estimator_type: str = "sft",
+        training_method: str = "sft",
         num_gpus: int = 8,
         gpu_memory: int = 85899345920,
         model_path: str = "ibm-granite/granite-3.3-8b-instruct",
@@ -299,13 +352,38 @@ def estimate(
         max_seq_len: int = None,
         max_tokens_per_gpu: int = None,
         use_liger: bool = False,
+        verbose: int = 1,
     ):
     """
     Convenience function for performing estimation
-    """
-    if estimator_type == "osft":
-        estimator = OSFTEstimator(num_gpus, gpu_memory, model_path, effective_batch_size, max_seq_len, max_tokens_per_gpu, use_liger)
-    else:
-        estimator = BasicEstimator(num_gpus, gpu_memory, model_path, effective_batch_size, max_seq_len, max_tokens_per_gpu, use_liger)
-    return estimator.estimate()
 
+    Args:
+        training_method (str): The training method to estimate the memory for. 
+                                Set to "osft" to estimate for OSFT,
+                                By default, SFT is assumed.
+        num_gpus (int): Number of GPUs to use for training (default: 8)
+        gpu_memory (int): The VRAM of each GPU in bytes (default: 85899345920 for 80 GB)
+        model_path (str): HuggingFace model path to the model to fine-tune
+                        (default: "ibm-granite/granite-3.3-8b-instruct")
+        effective_batch_size (int): The number of samples in a minibatch that the model
+                                    has to see before backpropping.
+        max_seq_len (int): Maximum sequence length of dataset samples 
+        max_tokens_per_gpu (int): The maximum number of tokens that can be placed
+                                on a single GPU during each mini-batch.
+        use_liger (bool): If true, estimate assuming Liger Kernels are used.
+        verbose (int): The level of verbosity to print out. Set to 0 for no printing,
+                        set to 1 to print out only hardware recommendations,
+                        set to 2 for a detailed memory breakdown.
+
+    Return:
+        A tuple containing three values:
+            lower_bound (int): The lower bound of the memory usage (in bytes)
+            expected (int): The expected amount of memory usage (in bytes)
+            upper_bound (int): The upper bound of the memory usage (in bytes)
+    """
+
+    if training_method.lower() == "osft":
+        estimator = OSFTEstimator(num_gpus, gpu_memory, model_path, effective_batch_size, max_seq_len, max_tokens_per_gpu, use_liger, verbose)
+    else:
+        estimator = BasicEstimator(num_gpus, gpu_memory, model_path, effective_batch_size, max_seq_len, max_tokens_per_gpu, use_liger, verbose)
+    return estimator.estimate()
